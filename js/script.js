005de41c87706ab254c28422d2c7be77c7d7c9c8
Himanshu Papola <71730231+himanshupapola@users.noreply.github.com>
2021-10-06 15:59:05 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.onreadystatechange = function() {
+	if (document.readyState !== "complete") {
+		document.querySelector("body").style.visibility = "hidden";
+		document.querySelector("#loader").style.visibility = "visible";
+	} else {
+		document.querySelector("#loader").style.display = "none";
+		document.querySelector("body").style.visibility = "visible";
+	}
+};
